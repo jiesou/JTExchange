bd492e0c3e78816f347196abf2cc9dff1655acc1
jiechus <84175239+jiesou@users.noreply.github.com>
2024-09-23 11:56:15 +0000

    fix: psql 数据结构，重新改用 Sequelize

--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,25 @@
 import express from 'express';
 import makeResponse from './units/makeResponse.js';
 import db from './adapter/db.js';
+import { DataTypes } from 'sequelize';
 import { configDotenv } from 'dotenv';
 import apiRouter from './api/index.js';
 
 configDotenv({
     path: ['.env.development.local', '.env']
 })
-const dbUser = new db('Users');
-const dbTransaction = new db('Transactions');
+const dbUser = new db('Users', {
+    key: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    pk: { type: DataTypes.STRING, unique: true, allowNull: false },
+    password: { type: DataTypes.STRING, allowNull: false },
+});
+const dbTransaction = new db('Transactions', {
+    key: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    from_pk: { type: DataTypes.STRING, allowNull: false },
+    to_pk: { type: DataTypes.STRING, allowNull: false },
+    amount: { type: DataTypes.FLOAT, allowNull: false },
+    time: { type: DataTypes.BIGINT, unique: true, allowNull: false },
+});
 
 const app = express();
 
