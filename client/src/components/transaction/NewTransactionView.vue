e694514060abafd67ceafcd711b44c9be5a4fb14
jiesou <84175239+jiesou@users.noreply.github.com>
2024-09-28 23:13:14 +0800

    fix: 交易提交

--- client/src/components/transaction/NewTransactionView.vue
+++ client/src/components/transaction/NewTransactionView.vue
@@ -17,11 +17,7 @@ const handleTransaction = transaction => {
   transactionState.loading = true;
   callApi('transaction/new', {
     method: 'POST',
-    body: {
-      amount: transaction.amount.value,
-      comment: transaction.comment.value,
-      to: transaction
-    }
+    body: JSON.stringify(transaction)
   }).then((res) => {
     transactionState.loading = false;
     message.success($t('dash.transferSuccess'));
@@ -37,13 +33,13 @@ const handleTransaction = transaction => {
   <a-form class="new-transaction" :model="transactionState" :label-col="{ span: 8 }" :wrapper-col="{ span: 16 }"
     @finish="handleTransaction">
     <a-form-item name="amount" :label="t('amount')" required>
-      <a-input-number :placeholder="$t('amount')" :controls=false></a-input-number>
+      <a-input-number v-model:value="transactionState.amount" :placeholder="t('amount')" :controls=false></a-input-number>
     </a-form-item>
     <a-form-item name="to" :label="t('dash.transferTo')" required>
-      <a-input-number :placeholder="t('dash.transferTo')" :controls=false></a-input-number>
+      <a-input-number v-model:value="transactionState.to" :placeholder="t('dash.transferTo')" :controls=false></a-input-number>
     </a-form-item>
-    <a-form-item name="comment" :label="$t('comment')">
-      <a-textarea :placeholder="t('comment')" allow-clear></a-textarea>
+    <a-form-item name="comment" :label="t('comment')">
+      <a-textarea v-model:value="transactionState.comment" :placeholder="t('comment')" allow-clear></a-textarea>
     </a-form-item>
     <a-button type="primary" html-type="submit" :loading="transactionState.loading">{{ t('transfer') }}</a-button>
   </a-form>
