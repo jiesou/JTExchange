0835adec6cbe55169f5dbef2b806a2cd7696db78
jiesou <84175239+jiesou@users.noreply.github.com>
2024-10-14 19:56:01 +0800

    opt: 解决余额累加时的浮点精度问题

--- server/src/units/transaction/fetchBalance.js
+++ server/src/units/transaction/fetchBalance.js
@@ -2,17 +2,18 @@ import { dbTransaction } from '../../index.js';
 
 async function fetchBalance(pk) {
     let balance = 0;
+    const precision = 1000000; // 10^6, 用于处理小数点后 6 位
 
     const expenditureTransaction = await dbTransaction.fetch({ from_pk: pk });
     expenditureTransaction.forEach((item) => {
-        balance -= item.amount;
+        balance -= Math.round(item.amount * precision);
     });
     const incomeTransaction = await dbTransaction.fetch({ to_pk: pk });
     incomeTransaction.forEach((item) => {
-        balance += item.amount;
+        balance += Math.round(item.amount * precision);
     });
 
-    return balance;
+    return balance / precision;
 }
 
 export default fetchBalance;
